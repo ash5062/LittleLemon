e90587b1d368cc71f3afa0c8729465d4788ead1e
Ash Chowdhury <ashchowdhury@ashs-air.myfiosgateway.com>
2023-07-03 11:07:33 -0400

    semantic structure added

---
+++ lemon-website/src/header.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+function Header(){
+    return (
+        <header>
+            <img src={logo.svg} alt="Little Lemon logo" />
+        </header>
+    );
+};
+
+export default Header;
